81dcfeb822ddcd112c0c3dfd8cb19a34dbb091ae
zwmiller <zaglamir@gmail.com>
2017-12-08 20:39:23 -0600

    move kde to neighbors, remove backup~ files

--- zwml/neighbors/__init__.py
+++ zwml/neighbors/__init__.py
@@ -1,5 +1,6 @@
 
 from .knn_classifier import knn_classifier
 from .knn_regressor import knn_regressor
+from .kde_approximator import kde_approximator
 
-__all__ = ['knn_classifier','knn_regressor']
+__all__ = ['knn_classifier','knn_regressor','kde_approximator']
--- zwml/utilities/__init__.py
+++ zwml/utilities/__init__.py
@@ -2,7 +2,6 @@
 from .data_splitting import *
 from .grid_search import *
 from .randomized_search import *
-from .kde_approximator import kde_approximator
 from .markov_chain import markov_chain
 
-__all__ = ['train_test_split','cross_val','grid_search','grid_search_cv','randomized_search','randomized_search_cv','kde_approximator','markov_chain']
+__all__ = ['train_test_split','cross_val','grid_search','grid_search_cv','randomized_search','randomized_search_cv','markov_chain']
