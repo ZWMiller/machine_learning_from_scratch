75610d0e0a974b48b169abdf374bdd5dd7ecc375
zwmiller <zaglamir@gmail.com>
2017-12-11 14:54:34 -0600

    update make_regression

--- zwml/datasets/make_regression.py
+++ zwml/datasets/make_regression.py
@@ -53,20 +53,48 @@ class make_regression:
             
         return X, y
     
-    def muck_up_data(self, X, dup_cols=True, add_nan=True):
+    def muck_up_data(self, X, dup_cols=True, add_nan=True, combine_feats=True):
         if dup_cols:
-            X = self.add_duplicate_columns(X)
+            X = self._add_duplicate_columns(X, dup_cols)
+        if combine_feats:
+            X = self._combine_features(X, combine_feats)
         if add_nan:
-            X = self.add_nans(X, add_nan)
+            X = self._add_nans(X, add_nan)
         return X
     
-    def add_duplicate_columns(self,X):
-        cols_to_dup = np.random.choice(np.arange(self.num_feats), np.random.randint(1,self.num_feats), replace=False)
+    def _add_duplicate_columns(self,X, dup_cols):
+        if isinstance(dup_cols, float):
+            num_to_dupe = int(dup_cols*X.size)   
+        elif isinstance(dup_cols, bool):
+            max_dupe = int((0.1*self.num_feats)+1.5)
+            num_to_dupe = np.random.randint(1,max_dupe)
+        elif isinstance(dup_cols, int):
+            num_to_dupe = dup_cols
+        else:
+            raise TypeError('dup_cols must be type float, int, or bool.')
+        
+        cols_to_dup = np.random.choice(np.arange(self.num_feats), num_to_dupe, replace=False)
         new_X = np.hstack((X, X.T[cols_to_dup].T.reshape(-1,len(cols_to_dup))))
         return new_X
             
-    def add_nans(self, X, add_nan_val):
+    def _combine_features(self, X, combine_feats):
+        if isinstance(combine_feats, float):
+            num_to_dupe = int(combine_feats*X.size) 
+        elif isinstance(combine_feats, bool):
+            max_dupe = int((0.1*self.num_feats)+1.5)
+            num_to_dupe = np.random.randint(1,max_dupe)
+        elif isinstance(combine_feats, int):
+            num_to_dupe = combine_feats
+        else:
+            raise TypeError('combine_feats must be type float, int, or bool.')
         
+        cols = np.random.choice(np.arange(self.num_feats), size=(num_to_dupe,2), replace=True)
+        for col_set in cols:
+            new_X = np.random.uniform(-1,1)*X.T[col_set[0]]+np.random.uniform(-1,1)*X.T[col_set[1]]
+            X = np.hstack((X, new_X.T.reshape(-1,1)))
+        return X
+    
+    def _add_nans(self, X, add_nan_val):
         if isinstance(add_nan_val, float):
             num_of_nans = int(add_nan_val*X.size)   
         elif isinstance(add_nan_val, int):
@@ -74,7 +102,7 @@ class make_regression:
         else:
             max_nans = int(0.1*X.size)
             num_of_nans = np.random.randint(1,max_nans)
-        print(num_of_nans)
+            
         for _ in range(num_of_nans):
             i = np.random.randint(0,X.shape[0])
             j = np.random.randint(0,X.shape[1])
