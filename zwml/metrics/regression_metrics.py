7d25fd6ea8a000c9d56fa15f914a9cba18586383
zwmiller <zaglamir@gmail.com>
2018-03-30 13:06:20 -0500

    add regression metrics notebook

--- zwml/metrics/regression_metrics.py
+++ zwml/metrics/regression_metrics.py
@@ -2,36 +2,52 @@ import numpy as np
 import pandas as pd
 
 def get_error(true,pred):
+    """
+    Returns predicted - true for each entry
+    """
     true = pandas_to_numpy(true)
     pred = pandas_to_numpy(pred)
     return pred-true
 
 def get_square_error(true,pred):
-    true = pandas_to_numpy(true)
-    pred = pandas_to_numpy(pred)
+    """
+    Returns the square of predicted - true for each entry
+    """
     return np.square(get_error(true,pred))   
 
 def mean_square_error(true, pred):
-    true = pandas_to_numpy(true)
-    pred = pandas_to_numpy(pred)
+    """
+    Returns the average predicted - true
+    """
     return np.mean(get_square_error(true,pred))
 
 def root_mean_square_error(true,pred):
-    true = pandas_to_numpy(true)
-    pred = pandas_to_numpy(pred)
+    """
+    Returns the sqrt of mean square error
+    """
     return np.sqrt(mean_square_error(true,pred))
 
 def mean_absolute_error(true,pred):
-    true = pandas_to_numpy(true)
-    pred = pandas_to_numpy(pred)
-    return np.mean(get_error(true,pred))
+    """
+    Returns the mean absolute value of error
+    """
+    return np.mean(np.abs(get_error(true,pred)))
 
 def sum_square_error(true,pred):
+    """
+    Returns the sum of squared errors
+    """
     true = pandas_to_numpy(true)
     pred = pandas_to_numpy(pred)
     return np.sum(get_square_error(true,pred))
 
 def r2_score(true,pred):
+    """
+    Returns R2 which is computed by
+    SSE = sum of squared errors from the model
+    SST = sume of squared errors to the mean of the data (y)
+    R2 = 1 - SSE/SST
+    """
     true = pandas_to_numpy(true)
     pred = pandas_to_numpy(pred)
     SSE = np.sum(get_square_error(true,pred))
@@ -39,22 +55,33 @@ def r2_score(true,pred):
     SST = np.sum(get_square_error(true,np.mean(true)*shpe))
     return 1.-(SSE/SST)
 
-def adj_r2(true, pred, num_data, num_features):
-    true = pandas_to_numpy(true)
-    pred = pandas_to_numpy(pred)
+def adj_r2(true, pred, X):
+    """
+    Returns a version of R2 that penalizes for having many
+    features. Fights against false correlations in data
+    and is generally better than R2.
+    """
+    X = pandas_to_numpy(X)
     rsquare = r2_score(true,pred)
+    num_data = X.shape[0]
+    num_features = X.shape[1]
     temp = (1-rsquare)*(num_data-1)
     temp = temp/(num_data-num_features-1)
     temp = 1 - temp
     return temp
 
 def assess_model(true, pred):
+    """
+    Computes a suite of metrics all at once
+    """
     true = pandas_to_numpy(true)
     pred = pandas_to_numpy(pred)
     return sum_square_error(true,pred), mean_square_error(true,pred), root_mean_square_error(true,pred)
 
 def test_regression_results(X, true, pred):
-    X = pandas_to_numpy(X)
+    """
+    A print out of many of the metrics that show model performance
+    """
     true = pandas_to_numpy(true)
     pred = pandas_to_numpy(pred)
     print("Mean Square Error: ", mean_square_error(true,pred))
@@ -62,10 +89,7 @@ def test_regression_results(X, true, pred):
     print("Mean Absolute Error: ",mean_absolute_error(true,pred))
     r2 = r2_score(true,pred)
     print("R2: ", r2)
-    try: 
-        print("Adj R2: ", adj_r2(true,pred,X.shape[0],X.shape[1]))
-    except: 
-        print("Adj R2: ", adj_r2(true, pred, X.shape[0],1))
+    print("Adj R2: ", adj_r2(true,pred,X))
 
 def pandas_to_numpy(x):
     """
